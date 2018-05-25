e37fe8c098969c57666432b5188ca303e526ef4d
James Haver <mchaver@gmail.com>
2018-05-25 15:41:26 +0800

    Init

---
+++ README.md
@@ -0,0 +1,9 @@
+# japonic-app
+
+```
+rustup update
+rustup toolchain install nightly
+rustup target add wasm32-unknown-unknown --toolchain nightly
+cargo +nightly build --target wasm32-unknown-unknown --release
+wasm-gc target/wasm32-unknown-unknown/release/japonic_app.wasm -o japonic_app.gc.wasm
+```
---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+use std::mem;
+use std::ffi::CString;
+use std::os::raw::{c_char, c_void};
+
+extern crate japonic;
+
+#[no_mangle]
+pub extern "C" fn alloc(size: usize) -> *mut c_void {
+    let mut buf = Vec::with_capacity(size);
+    let ptr = buf.as_mut_ptr();
+    mem::forget(buf);
+    return ptr as *mut c_void;
+}
+
+#[no_mangle]
+pub extern "C" fn dealloc_str(ptr: *mut c_char) {
+    unsafe {
+        let _ = CString::from_raw(ptr);
+    }
+}
+
+#[no_mangle]
+pub fn romaji_to_hiragana(ptr: *mut c_char) -> *mut c_char {
+    let mut s: String;
+
+    unsafe {
+        s = CString::from_raw(ptr).into_string().unwrap();
+    }
+    
+    s = japonic::standard::lib::romaji_to_hiragana(&s);
+
+    let c_string = CString::new(s).unwrap();
+    c_string.into_raw()
+}
